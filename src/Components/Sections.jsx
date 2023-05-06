a2a14bacb7f1c2006ea2c6a884dc102e7647e83d
joel <jsmblog19@gmail.com>
2023-05-06 11:29:05 -0500

    [fixed] animations

--- src/Components/Sections.jsx
+++ src/Components/Sections.jsx
@@ -7,7 +7,8 @@ return (
         </div>
         <article>
             <div className="ContSectionsArt">
-                <div data-aos="fade-right" data-aos-easing="linear" data-aos-duration="1500" className="ContDataThemes">
+                <div data-aos="fade-up" data-aos-easing="linear"
+     data-aos-duration="1500" className="ContDataThemes">
                     <h3>VARIABLES AND DATA TYPES</h3>
                     <h3>LOGICAL OPERATORS</h3>
                     <h3>CONTROL STRUCTURES</h3>
@@ -15,7 +16,8 @@ return (
                     <h3>ARRAYS</h3>
                     <h3>DATA STRUCTURES</h3>
                 </div>
-                    <div data-aos="fade-left"  data-aos-easing="linear"  data-aos-duration="1500" className="ContCreatorNews">
+                    <div data-aos="fade-down" data-aos-easing="linear"
+     data-aos-duration="1500"  className="ContCreatorNews">
                         <div><h3>{`Let's know her story`}</h3></div>
                         <div className="ImgCreatorAndSms">
                             <div>
